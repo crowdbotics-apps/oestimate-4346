c5f324929e21f1e0fac874f09f7b949a1a474d0d
Crowdbotics <team@crowdbotics.com>
2019-06-06 21:43:25 +0000

    remove package django-bootstrap4

--- home/views.py
+++ home/views.py
@@ -8,7 +8,6 @@ from home.models import CustomText, HomePage
 def home(request):
     packages = [
 	{'name':'eth-utils', 'url': 'http://pypi.python.org/pypi/eth-utils/0.7.4'},
-	{'name':'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
     ]
     context = {
